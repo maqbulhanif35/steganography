5cf24f1ef8424a12a529282a8da51d0755e51bde
maqbulhanif35 <100834678+maqbulhanif35@users.noreply.github.com>
2024-11-17 13:16:58 +0300

    Add files via upload
    Uploaded source files

---
+++ extract.c
@@ -0,0 +1,443 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+uint8_t PNG [8]= { 137, 80, 78, 71, 13, 10, 26, 10};
+unsigned char *IEND = "IEND";
+char *STEGSIG = "o90xpx95";
+int numOfFiles;
+uint8_t f = 0; //0 for files 1 for folder
+//read signature
+char *slashPath(char *name){//culls string at the first /
+	char *folderName;
+	uint8_t slash = 0;
+	int len = strlen(name);
+	for (size_t i = 0; i < len; i++)
+	{
+		if (name[i] == '/') slash = i+1;
+		
+	}
+	folderName = (char*)malloc((len-slash)+1);
+	int t = 0;
+	for (size_t i = slash; i < len; i++)
+	{
+		folderName[t] = name[i];
+		t++;
+	}
+	folderName[t] = '\0';
+	return folderName;
+}
+void readSig(FILE *file){//this function verifies png file 
+    if(file == NULL){
+        fprintf(stderr,"ERROR: Could not open File!");
+        exit(1);
+    }
+    unsigned char *buffer = (unsigned char*)malloc(8);//8 bytes is the size of the png signatuer
+    int bytesRead = fread(buffer,1,8,file);
+
+    if(bytesRead==0){//could not read file
+        fprintf(stderr,"ERROR: Could not read PNG header!");
+        exit(1);
+    }
+    int status = memcmp(PNG,buffer,8);
+    if (status!=0)
+    {
+        fprintf(stderr,"ERROR: File is not a PNG file!");
+        exit(1);
+    }
+	free(buffer);
+    
+}
+int checkIfFolderExists(char *path){
+    int result = 0;
+    struct stat statbuf;
+    if(stat(path, &statbuf) != 0){
+        result = 0;
+    }else{
+        result = 1;
+    }
+    return result;
+}
+int readChunk(FILE *file,int offset,char *data){//returns location of STEGSIG signature given offset
+	//get size
+    uint8_t isValidSteg = 0;
+	int location = -1;
+	fseek(file,0,SEEK_END);
+	int size=ftell(file);
+	fseek(file,offset,SEEK_SET);
+    
+    int size2=size;
+    size=size-offset;
+
+
+
+	unsigned char *temp = (unsigned char*)malloc(8);//stores STEGSIG
+	for (int i = offset; i < size2; i++)
+	{
+		if(i<=(size2-8)){
+			for (int j = 0; j < 8; j++)
+			{
+                
+				temp[j]=data[i+j];
+                
+			}
+		}
+		//check if file has STEGSIG 
+		if(memcmp(temp,STEGSIG,8)==0){
+            isValidSteg = 1;
+            location = i;
+            return location;
+		}
+	}
+    free(temp);
+	return location;
+}
+int checkIfFileExists(char *fileName){
+    FILE* file;
+    if (file=fopen(fileName,"r"))
+    {
+        fclose(file);
+        return 1;
+    }
+    return 0;
+    
+}
+void السلام(char *fileName,char *data,long long int size){//receives file name and data and creates a file with the filename and writes the data to it
+    //first check if file exists
+    char yesorno;
+    printf("INFO: EXTRACTING %s\n",fileName);
+    if(checkIfFileExists(fileName)==1){
+        printf("The File %s Already Exists, Do you want to overwrite(y/n)\n",fileName);
+        scanf(" %c", &yesorno);
+        if(yesorno == 'n' || yesorno == 'N'){
+            return;//exit without writing anything
+        }
+    }
+    FILE *file = fopen(fileName,"wb");
+    if(file == NULL){
+        fprintf(stderr,"ERROR: Failed to create File For writing, %s",fileName);
+        exit(1);
+    }
+    fwrite(data,1,size,file);
+    fclose(file);
+}
+int readData(FILE* pngFile,long long int location,long long int location2){
+    //reads the data between the given offsets
+    location+=8;//exclude STEGSIG
+    fseek(pngFile,location,SEEK_SET);
+    location2 = location2 - 4;//skip ENDSIG
+    //
+    int fileNameSize=0;
+    fread(&fileNameSize,4,1,pngFile);
+    location+=4;
+    if (fileNameSize <= 0) {
+        fprintf(stderr, "ERROR: UNEXPECTED SIZE: %d\n", fileNameSize);
+        return -1;
+    }
+    //
+
+    char *fileName = (char*)malloc(fileNameSize+1);
+    long long int tf = fread(fileName,1,fileNameSize,pngFile);
+    fileName[fileNameSize] = '\0';
+    location+=fileNameSize;
+    if(f == 0){
+        fileName = slashPath(fileName);
+    }
+    if (fileName == NULL) {
+        perror("malloc failed for fileName");
+        return -1;
+    }
+    if (tf!=fileNameSize)
+    {//failed STEGSIG may indicate false signature
+        printf("Error: UNEXPECTED SIZE!\n");
+        free(fileName);
+        return -1;
+    }
+    //extract foldername 
+    char *folderName;
+    int slash = 0;
+    for (size_t i = 0; i < fileNameSize; i++)
+    {
+        if(fileName[i] == '/')slash = i+1;//the last slash representing the complete folder structure
+    }
+    folderName = (char*)malloc(slash+2);
+    snprintf(folderName,slash+1,"%s",fileName);
+    //
+    for (size_t i = 0; i < strlen(folderName); i++)
+    {
+        if(folderName[i] == '/'){
+            char *temp = (char*)malloc(i+3);
+            snprintf(temp,i+1,"%s",folderName);
+            if( checkIfFolderExists(temp) == 0){
+                mkdir(temp,0777);
+            }else{
+            }
+            free(temp);
+        }
+    }
+    //if(checkIfFolderExists(folderName) == 0)mkdir(folderName,0777);
+    free(folderName);
+    //
+    long long int sizeOfData = location2 - location;
+    char *data = (char*)malloc(sizeOfData);
+    long long int read = fread(data,1,sizeOfData,pngFile);
+    
+    if(read!=sizeOfData){//failed STEGSIG may indicate false signature
+        printf("ERROR: UNEXPECTED SIZE!\n");
+        free(data);
+        free(fileName);
+        return -1;
+    }
+    
+    السلام(fileName,data,sizeOfData);
+    free(fileName);
+    free(data);
+    return 0;
+}
+int writeData(FILE* pngFile){
+	//read data to be written
+    //get size
+    fseek(pngFile,0,SEEK_END);
+    long long int size = ftell(pngFile);//go to end
+    fseek(pngFile,0,SEEK_SET);//return to start
+    char *buffer = (char*)malloc(size);
+    long long int dataRead = fread(buffer,1,size,pngFile);
+    if(dataRead!=size){
+        fprintf(stderr,"ERROR: Data Read is not Equal to file size!");
+        exit(1);
+    }
+    long long int location = readChunk(pngFile,0,buffer); //will give us locations of STEGSIG
+                                        //start reading from there
+    long long int location2 ;
+    unsigned char *temp = (unsigned char*)malloc(8);
+    long long int start = location;
+    char preventFalseFlag[4];
+	for (long long int i = start+1; i < size; i++)
+	{
+		if(i<=(size-8)){
+			for (int j = 0; j < 8; j++)
+			{
+				temp[j]=buffer[i+j];
+			}
+		}
+		if (memcmp(temp,STEGSIG,8) == 0)
+		{
+			//note the location
+            location2 = location; //range from location2 to location
+			location = i;
+            //signature verification --prevent false flags
+            char IEND[4] = {0xE7,0xF2,0x6E,0x96};
+            for (size_t j = 0; j < 4; j++)
+            {
+                preventFalseFlag[j] = buffer[i-(4-j)];
+            }
+            if(memcmp(IEND,preventFalseFlag,4) == 0){//verified
+                if(readData(pngFile,location2,location) == 0)numOfFiles++;
+            }else{
+                continue;//skip the affected file
+            }
+		}
+	}
+    if(location != -1){
+        readData(pngFile,location,size);
+        numOfFiles++;
+    }
+    free(temp);
+    free(buffer);
+}
+int readNumOfFiles(FILE* pngFile){
+    int result = 0;
+    int location;
+    //get size of pngFile
+	fseek(pngFile,0,SEEK_END);
+	int size=ftell(pngFile);
+	fseek(pngFile,0,SEEK_SET);
+
+    //read data from file in blocks for efficient memory usage ->TODO
+	unsigned char *buffer =(unsigned char*)malloc(size);//not good for large file sizes
+	int status = fread(buffer,1,size,pngFile);
+
+	if(status!=size){
+		printf("ERROR: UNEXPECTED SIZE\n");
+		exit(1);
+	}
+	//loop through buffer to check for STEGSIG signature
+	unsigned char *temp = (unsigned char*)malloc(8);;
+	for (int i = 0; i < size; i++)
+	{
+		if(i<=(size-8)){
+			for (int j = 0; j < 8; j++)
+			{
+				int t=i+j;
+				temp[j]=buffer[i+j];
+				//printf("%x\n",temp);
+			}
+		}
+		//printf("%s\n",temp);
+		if (memcmp(temp,STEGSIG,8) == 0)
+		{
+			//note the location
+			location = i;
+            result +=1 ;
+		}
+	}
+	free(temp);
+	return result;
+}
+int checkIfFileOrFolder(char *name){
+	struct stat fileStat;
+	if(stat(name,&fileStat)<0){
+		return -1;
+	}
+	if(S_ISDIR(fileStat.st_mode)){
+		return 1;//1 folder 0 for file
+	}else{
+		return 0;
+	}
+}
+char *getPath(char *name){
+    char *result;
+    int length = strlen(name);
+    int location = 1;
+    int previousLocation = 0;
+    result = (char*)malloc(length);
+    for (size_t i = 0; i < length; i++)
+    {
+        if(name[i] == '/')location = i;
+    }
+    result = (char*) malloc(location);
+    for (size_t i = 0; i < location; i++)
+    {
+        result[i] = name[i];
+    }
+    return result;
+}
+
+
+int getIEND(FILE *file){//returns location of IEND signature
+	//get size
+	int location;
+	fseek(file,0,SEEK_END);
+	int size=ftell(file);
+	fseek(file,0,SEEK_SET);
+	unsigned char *buffer =(unsigned char*)malloc(size);
+	int status = fread(buffer,1,size,file);
+    
+	if(status!=size){
+		printf("ERROR: UNEXPECTED NUMBER OF BYTES\n",status,size);
+		exit(1);
+	}
+	//loop through buffer to check for IEND signature
+	unsigned char *temp = (unsigned char*)malloc(4);;
+	for (int i = 0; i < size; i++)
+	{
+		if(i<=(size-4)){
+			for (int j = 0; j < 4; j++)
+			{
+				temp[j]=buffer[i+j];
+			}
+		}
+		if (memcmp(temp,IEND,4)==0)
+		{
+			//note the location
+			location = i;
+            break;
+		}
+	}
+	free(temp);
+    free(buffer);
+	return location;
+}
+int searchArgs(char **args,char *input,int len){//searches args for input string
+    for (size_t i = 0; i < len; i++)
+    {
+        if(strcmp(args[i],input) == 0){
+            return 1;
+        }
+    }
+    return 0;
+}
+int main(int argc, char  *argv[])
+{
+    argc-=1;
+    char **strings = (++argv);
+    char clean = 'b';
+    //check flags
+    if(searchArgs(strings,"--clean",argc) == 1)clean = 'y';
+    //open pngFile
+    FILE* checkExistence = fopen(strings[0],"r");
+    if(checkExistence == NULL){
+        perror("ERROR");
+        exit(1);
+    }
+    fclose(checkExistence);
+    FILE* pngFile = fopen(strings[0],"r+b");
+    if(pngFile == NULL){
+        printf("ERROR: Could not Open PNG!");
+        perror("ERROR");
+        exit(1);
+    }
+    //verify png 
+    readSig(pngFile);
+    char *folderName = getPath(strings[0]);
+    chdir(folderName);
+    long long int globalLocation = getIEND(pngFile) +4;
+    fseek(pngFile,0,SEEK_END);
+    long long int sizeOfPngFile = ftell(pngFile);
+    fseek(pngFile,0,SEEK_SET);
+    if(globalLocation+4 == sizeOfPngFile){//check if there is any embedded files
+        printf("NO FILES FOUND!\n");
+        return 1;
+    }
+    //check if simply files or folder
+    long long int location = getIEND(pngFile);//returns location of IEND + 4
+    {
+        char *stegHolder = malloc(8);
+        fseek(pngFile,location+8,SEEK_SET);
+        fread(stegHolder,1,8,pngFile);
+        fseek(pngFile,0,SEEK_SET);
+        if(memcmp(stegHolder,STEGSIG,8) == 0){
+            
+        }else{
+            f = 1;
+            fseek(pngFile,location+8,SEEK_SET);
+            int folderNameSize;
+            char *nameOfFolder;
+            fread(&folderNameSize,sizeof(int),1,pngFile);
+            nameOfFolder = (char*)malloc(folderNameSize);
+            fread(nameOfFolder,1,folderNameSize,pngFile);
+            nameOfFolder[folderNameSize] = '\0';
+            printf("INFO: EXTRACTING FILES TO  : %s\n",nameOfFolder);
+            if(checkIfFolderExists(nameOfFolder) == 1){printf("FOLDER %s ALREADY EXISTS\n"); return 1;}
+            else mkdir(nameOfFolder,0777);
+        }
+        free(stegHolder);
+    }
+    
+    writeData(pngFile);
+	//clean pngFile
+	if(clean == 'y' || clean == 'Y'){
+        globalLocation = globalLocation + 4;
+        char *data = (char*)malloc(globalLocation);
+        fseek(pngFile,0,SEEK_SET);
+        fread(data,1,globalLocation,pngFile);
+        fseek(pngFile,0,SEEK_SET);
+        fclose(pngFile);
+        char *fileName = slashPath(strings[0]);
+        FILE* cleaner = fopen(fileName,"wb");
+        if (data == NULL)
+        {
+            printf("ERROR: COULD NOT CLEAN FILE!\n");
+            return 1;
+        }
+        fseek(cleaner,0,SEEK_SET);
+		int written = fwrite(data,1,globalLocation,cleaner);
+        fclose(cleaner);
+        free(data);
+	}
+    printf("SUCCESFULLY EXTRACTED %d FILES\n",numOfFiles);
+    return 0;
+}
---
+++ png.c
@@ -0,0 +1,441 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
+int fileCount = 0;
+uint8_t f = 0;//stores whether the folder argument is on or off
+uint8_t type =0;//0 for has no embedded data 1 for has data
+uint8_t PNG [8]= {137, 80, 78, 71, 13, 10, 26, 10};//png header
+unsigned char *IEND = "IEND";
+//char *STEGSIG = "o90xpx95";
+char ENDSIG[4] = {0xE7,0xF2,0x6E,0x96};
+char *STEGSIG = "o90xpx95";
+long long int globalLocation = 0;
+char *slashPath(char *name){
+	char *folderName;
+	uint8_t slash = 0;
+	int len = strlen(name);
+	for (size_t i = 0; i < len; i++)
+	{
+		if (name[i] == '/') slash = i+1;
+		
+	}
+	folderName = (char*)malloc((len-slash)+1);
+	int t = 0;
+	for (size_t i = slash; i < len; i++)
+	{
+		folderName[t] = name[i];
+		t++;
+	}
+	folderName[t] = '\0';
+	return folderName;
+}
+int checkIfFileOrFolder(char *name){
+	struct stat fileStat;
+	if(stat(name,&fileStat)<0){
+		return -1;
+	}
+	if(S_ISDIR(fileStat.st_mode)){
+		return 1;//1 folder 0 for file
+	}else{
+		return 0;
+	}
+}
+//read signature
+void readSig(FILE *file){//this function verifies png file 
+    if(file == NULL){
+        fprintf(stderr,"ERROR: Could not open File");
+        exit(1);
+    }
+    unsigned char *buffer = (unsigned char*)malloc(8);//8 bytes is the size of the png signatuer
+    int bytesRead = fread(buffer,1,8,file);
+    
+    if(bytesRead==0){//could not read file
+        fprintf(stderr,"ERROR: Could not read PNG header!");
+        exit(1);
+    }
+    int status = memcmp(PNG,buffer,8);
+    if (status!=0)
+    {
+        fprintf(stderr,"File is not a PNG file! First Argument Should Always be the png file");
+        exit(1);
+    }
+	free(buffer);
+    
+}
+int checkEmbeddings(FILE* pngFile,unsigned char *data){
+	fseek(pngFile,0,SEEK_END);
+	int size=ftell(pngFile);
+	fseek(pngFile,0,SEEK_SET);
+	char *buffer = (char*)malloc(8);//for STEGSIG
+	for (int i = 0; i < size; i++)
+	{
+		if(i<=(size-8)){
+			for (int j = 0; j < 8; j++)
+			{
+				buffer[j]=data[i+j];
+			}
+		}
+		if (memcmp(buffer,STEGSIG,8)==0)
+		{
+			//if found return 1 
+			return 1;
+		}
+	}
+	free(buffer);
+	return 0;
+}
+int readChunk(FILE *file){//returns location of IEND signature
+	//get size
+	int location;
+	fseek(file,0,SEEK_END);
+	int size=ftell(file);
+	fseek(file,0,SEEK_SET);
+	unsigned char *buffer =(unsigned char*)malloc(size);
+	int status = fread(buffer,1,size,file);
+
+	if(status!=size){
+		printf("ERROR: UNEXPECTED NUMBER OF BYTES\n");
+		exit(1);
+	}
+	//loop through buffer to check for IEND signature
+	unsigned char *temp = (unsigned char*)malloc(4);;
+	for (int i = 0; i < size; i++)
+	{
+		if(i<=(size-4)){
+			for (int j = 0; j < 4; j++)
+			{
+				int t=i+j;
+				temp[j]=buffer[i+j];
+			}
+		}
+		if (memcmp(temp,IEND,4)==0)
+		{
+			//note the location
+			location = i;
+		}
+	}
+	//checkEmbeddings(file,buffer);?
+	free(temp);
+	return location;
+}
+int writeData(char *fileName,FILE* pngFile){
+	char *fileNameF;
+    long long int location = globalLocation;
+    FILE* dataFile = fopen(fileName,"rb");
+    if(dataFile==NULL){
+        fprintf(stderr,"ERROR: Could not open File %s\n",fileName);
+        return -1;
+    }
+	//read data to be written
+	fseek(dataFile,0,SEEK_END);
+	long long int size=ftell(dataFile);
+	unsigned char *data = (unsigned char*)malloc(size);
+	if(data == NULL){
+		printf("ERROR: COULD NOT ALLOCATE MEMORY \n");
+		exit(1);
+	}
+	fseek(dataFile,0,SEEK_SET);
+    long long int read = fread(data,1,size,dataFile);
+	//first write STEG signature
+	location = location+4;
+	fseek(pngFile,location,SEEK_SET);
+	long long int stegWrite = fwrite(STEGSIG,1,8,pngFile);
+	location = location+8;
+    //write file name and size of fileName
+    int fileNameSize = strlen(fileName);
+	fseek(pngFile,location,SEEK_SET);
+    fwrite(&fileNameSize,(sizeof(int)),1,pngFile);//write size of the filename
+	location = location+4;
+	fseek(pngFile,location,SEEK_SET);
+	fwrite(fileName,1,fileNameSize,pngFile);
+	location = location+fileNameSize;
+    //
+	//write data
+	long long int written = fwrite(data,1,size,pngFile);
+	fseek(pngFile,location+8,SEEK_SET);
+	if(size!=written){
+		printf("ERROR: UNEXPECTED NUMBER OF BYTES\n");
+		exit(1);
+	}
+    //write closing signature
+    fwrite(ENDSIG,1,4,pngFile);
+    fclose(dataFile);
+	free(data);
+	fileCount++;
+	return 0;
+	//the file pointer will be left for the next iteration of the function
+	//it will point to the end of the file
+}
+void addFolder(FILE* pngFile,char* folderName){//adds the specified folder to the pngFile
+	struct dirent *entry;
+	int fileCount = 0;
+	DIR *dir = opendir(folderName);
+	if(dir == NULL){
+		printf("ERROR : %s %s\n",strerror(errno),folderName);
+		exit(1);
+		}
+		size_t len2 = 0;
+		while((entry = readdir(dir)) != NULL){ 
+			if (entry->d_type == DT_REG)
+			{
+				size_t len = 0;
+				char *completeFileName;
+				len = strlen(folderName) + strlen(entry->d_name) +2;
+				completeFileName = (char*)malloc(len);
+				snprintf(completeFileName,len,"%s/%s",folderName,entry->d_name);
+				if( writeData(completeFileName,pngFile) == 0 ){
+					printf("INFO: ADDED %s\n",completeFileName);
+				}else{
+					printf("ERROR: COULD NOT ADD %s\n",completeFileName);
+				}
+				free(completeFileName);
+			}else if (entry->d_type == DT_DIR)
+			{
+				char *completeFolderName;
+
+				len2 = strlen(folderName) + strlen(entry->d_name) + 3;
+				completeFolderName = (char*)malloc(len2);
+				snprintf(completeFolderName,len2,"%s/%s",folderName,entry->d_name);
+				if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+				{
+					continue;
+				}else addFolder(pngFile,completeFolderName);
+				free(completeFolderName);
+			}
+			}
+	closedir(dir);
+}
+void deleteFolder(char *folderName){//deletes specified folder
+			struct dirent *entry;
+			DIR *folder;
+			folder = opendir(folderName);
+			if (folder == NULL)
+			{
+				perror("ERROR COULD NOT DELETE FOLDER:");
+				exit(0);
+			}
+			while((entry = readdir(folder))!=NULL){
+				if (entry->d_type == DT_REG)
+				{
+					char *fullPath;
+					fullPath = (char*)malloc(strlen(folderName)+strlen(entry->d_name)+2);
+					sprintf(fullPath,"%s/%s",folderName,entry->d_name);
+					if(remove(fullPath)!= 0){
+						perror("ERROR REMOVNG FILE");
+						printf("%s\n",entry->d_name);
+					}else{
+						printf("INFO: REMOVING %s \n",entry->d_name);
+					}
+					free(fullPath);
+				}else if (entry->d_type == DT_DIR)
+				{
+					char *fullPathT;
+					int len2 = strlen(folderName) + strlen(entry->d_name) + 3;
+					fullPathT = (char*)malloc(len2);
+					sprintf(fullPathT,"%s/%s",folderName,entry->d_name);
+					if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)continue;
+					else{
+					    deleteFolder(fullPathT);
+					    free(fullPathT);
+					}
+				}
+					
+			}
+			if(rmdir(folderName) != 0){
+				perror("ERROR DELETING FOLDER\n");
+			}else{
+				printf("INFO: DELETED FOLDER %s\n",folderName);
+			}
+			closedir(folder);
+			
+}
+int searchArgs(char **args,char *input,int len){//searches args for input string
+    for (size_t i = 0; i < len; i++)
+    {
+        if(strcmp(args[i],input) == 0){
+            return 1;
+        }
+    }
+    return 0;
+}
+int main(int argc, char  *argv[])
+{
+
+    argc = argc-1;
+	int folderIndex;
+	if(argc <= 1){printf("USAGE: png pngFile file1 file2...\n");return 1;}//incorrect usage error
+	char *name = (char *) malloc(20);
+    //array of strings
+    char **strings = (++argv);//this array of strings contains the file names
+                              //the first element is always the png file
+        //open png file
+    FILE *pngFile = fopen(strings[0],"ab+");
+    if(pngFile == NULL){
+        fprintf(stderr,"ERROR: Could not open File %s", strings[0]);
+        exit(1);
+    }
+	//check validity of pngFile
+	readSig(pngFile);
+	//check for the --f flag
+	if (searchArgs(argv,"-f",argc) == 1)
+	{
+			f=1;
+	}
+	//loop through the args to find the folder
+	if (f == 1)
+	{
+		fseek(pngFile,0,SEEK_END);
+		int sizeOfImage = ftell(pngFile);
+		fseek(pngFile,0,SEEK_SET);
+		//check if cover image already contains a folder
+		int location = readChunk(pngFile) + 8;
+		if(location >= sizeOfImage){
+
+		}else{//all this to check if the cover image has a folder or files embedded in it
+			char *checker = (char*)malloc(8);
+			//if immediately after IEND there is not stegsig then it is a folder
+			int read = fread(checker,1,8,pngFile);
+			if(memcmp(checker,STEGSIG,8) == 0){//it is a file
+			}else{
+				printf("ERROR: CANNOT ADD FOLDER AS IMAGE ALREADY CONTAINS EMBEDDED FILES!\n");
+			    return 1;
+			}
+		    fseek(pngFile,0,SEEK_SET);
+		    free(checker);
+		}
+		int t;
+		for (size_t i = 0; i < argc; i++)
+		{
+			t = checkIfFileOrFolder(strings[i]);
+			if (t == 1)
+			{
+				printf("INFO: ADDING FOLDER: %s\n",strings[i]);
+				folderIndex = i;
+				break;
+			}
+		}
+		if(t!=1){
+			printf("USAGE: png pngFile -f folder \n");//incorrect usage error
+			exit(1);
+		}
+	}else{
+		int t;
+		for (size_t i = 0; i < argc; i++)
+		{
+			t = checkIfFileOrFolder(strings[i]);
+			if (t == 1)
+			{
+				printf("ERROR: Incorrect usage, to add a folder use the -f flag");
+				return 1;
+			}
+		}
+		
+	}
+
+    fseek(pngFile,0,SEEK_SET);
+	globalLocation = readChunk(pngFile);
+	
+    //write each file in strings to pngFile using funciton writeData 
+	//FOR REGULAR FILES
+	if(f == 0){//adding files
+		for (size_t i = 1; i < argc; i++)
+		{
+			if(writeData(strings[i],pngFile) == 0){
+				printf("INFO: ADDED FILE %s\n",strings[i]);
+			}else{
+				printf("ERROR: COULD NOT ADD FILE %s\n",strings[i]);
+			}
+		}
+	}
+	else if(f == 1){//adding folder
+	    char  *folderName = slashPath(strings[folderIndex]);
+		folderName = slashPath(strings[folderIndex]);
+	    int len = strlen(strings[folderIndex]);
+		char *baseFolder;
+		int slash = 0;
+		for (size_t i = 0; i < len; i++)
+		{
+			if(strings[folderIndex][i] == '/'){
+				slash = i+2;
+			}
+		}
+		baseFolder = (char*)malloc(slash+1);
+		snprintf(baseFolder,slash,"%s",strings[folderIndex]);
+		int changeDir = -1;
+		int baseFolderLen = 2;
+		baseFolderLen = strlen(baseFolder);
+		if( baseFolderLen < 1 ){//we are in the parent dir
+		    changeDir = 0;
+			DIR *dir = opendir(folderName);
+			if(dir == NULL){
+				printf("ERROR : %s %s\n",strerror(errno),folderName);
+				closedir(dir);
+		        return 1;
+			}
+			closedir(dir);
+		}
+		    
+		else{//not in parent dir
+			changeDir = chdir(baseFolder);
+			DIR *dir = opendir(folderName);
+			if(dir == NULL){
+				printf("ERROR : %s %s\n",strerror(errno),folderName);
+				closedir(dir);
+				return 1;
+			}
+			closedir(dir);
+		}
+		if(changeDir == -1){
+			perror("ERROR CHANGING FOLDER : ");
+			return 1;
+		}
+	    //
+		fseek(pngFile,globalLocation+4,SEEK_SET);
+		fwrite(&len,4,1,pngFile);
+        fwrite(folderName,1,len,pngFile);
+		fseek(pngFile,0,SEEK_SET);
+		//
+		addFolder(pngFile,folderName);
+		free(folderName);
+	}
+	fclose(pngFile);
+	if(f == 0){//deleting files
+		printf("Remove original files? (y/n)\n");
+		char deleteOrNot;
+		scanf("%c",&deleteOrNot);
+		if (deleteOrNot == 'y' || deleteOrNot == 'Y')
+		{
+			{
+				for (size_t i = 1; i < argc; i++)
+				{
+					if(remove(argv[i]) == 0){
+						printf("DELETING %s\n",argv[i]);
+					}else{
+						perror("ERROR: ");
+					}
+				}
+				
+			}
+		}
+		
+	}
+
+	else if(f == 1){//deleting folder
+	    char  *folderName = slashPath(strings[folderIndex]);
+		printf("Remove original folder? (y/n)\n");
+		char deleteOrNot;
+		scanf("%c",&deleteOrNot);
+		if(deleteOrNot == 'y' || deleteOrNot == 'Y'){
+			printf("DELETING FOLDER %s\n",strings[folderIndex]);
+			deleteFolder( folderName );
+		}
+		free(folderName);
+	}
+	printf("INFO: ADDED %d files",fileCount);
+	return 0;
+}
